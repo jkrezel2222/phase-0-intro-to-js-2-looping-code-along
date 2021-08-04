769413f96304b907ab3c4a9a81ba7bb0d85dc021
Jannette <jannette.krezel@gmail.com>
2021-08-04 20:56:38 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const guestNames = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(guestNames, surprise) {
+    let thankYouCards = [];
+    for (let i = 0; i <guestNames.length; i++) {
+        thankYouCards.push(`Thank you, ${guestNames[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
